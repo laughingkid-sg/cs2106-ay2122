9733a2ada44d2480e3560e44be525088ae49e047
Goh Zheng Teck <adamgoh1998@gmail.com>
2021-08-31 23:59:04 +0800

    Lab1 ex6

--- lab1/ex6/check_syscalls.sh
+++ lab1/ex6/check_syscalls.sh
@@ -2,9 +2,9 @@
 
 ####################
 # Lab 1 Exercise 6
-# Name:
+# Name: Goh Zheng Teck
 # Student No:
-# Lab Group: 
+# Lab Group: 08
 ####################
 
 echo "Printing system call report"
@@ -13,3 +13,4 @@ echo "Printing system call report"
 gcc -std=c99 pid_checker.c -o ex6
 
 # Use strace to get report
+strace -c ./ex6
